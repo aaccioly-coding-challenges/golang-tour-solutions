86397c5083b63f609859febcad55c9091e2ef6f1
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-04 01:17:33 +0100

    feat(flowcontrol): add for as while loop program and test

---
+++ flowcontrol/for-continued/for-continued_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+	"testing"
+)
+
+func TestForContinuedProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+
+	// The program outputs the sum of numbers until it reaches or exceeds 1000
+	expectedOutput := "1024\n"
+	if output != expectedOutput {
+		t.Errorf("Expected output %q, got %q", expectedOutput, output)
+	}
+}
---
+++ flowcontrol/for-is-gos-while/for-is-gos-while_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+	"testing"
+)
+
+func TestForIsGoWhileProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "1024\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
