5a5b4e28f7e9a78e51d518a70dd492a207aa2da1
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-13 23:33:32 +0100

    feat(moretypes): add struct pointers code and test
    - Introduced a new Go file demonstrating struct pointers
    - Added a test to verify output of the main function

---
+++ moretypes/struct-pointers/struct-pointers_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestStructPointesProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "{1000000000 2}\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
