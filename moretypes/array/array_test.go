d686dd6550ac1f140ff461daef75a249597e88c8
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-15 00:02:40 +0100

    feat(moretypes): add array code and test
    - Implement main function with string and integer arrays
    - Add test for verifying output of the array program

---
+++ moretypes/array/array_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestArrayProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "Hello World\n[Hello World]\n[2 3 5 7 11 13]\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
