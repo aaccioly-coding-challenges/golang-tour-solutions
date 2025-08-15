09173511eb7e1939514a700cc3cea18e6ebf3014
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-16 00:02:07 +0100

    feat(moretypes): add slice length and capacity program and test
    - Implemented a main function to demonstrate slice length and capacity
    - Added a test to validate the output of the main function

---
+++ moretypes/slice-len-cap/slice-len-cap_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestSliceLengthAndCapacityProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "len=6 cap=6 [2 3 5 7 11 13]\nlen=0 cap=6 []\nlen=4 cap=6 [2 3 5 7]\nlen=2 cap=4 [5 7]\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
