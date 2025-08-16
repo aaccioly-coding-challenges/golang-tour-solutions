5c4522793f74ba9f27694ea548cc023639beeb06
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-16 01:06:48 +0100

    feat(moretypes): add slices of slices code and test
    - Implement a simple tic-tac-toe game using slices of slices
    - Add a test to validate the output of the main function

---
+++ moretypes/slices-of-slice/slices-of-slice_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestSlicesOfSlices(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "X _ X\nO _ X\n_ _ O\n"
+
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
