0f27d706fbea3b236e3ce17b70dcca5bc0fba458
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-20 00:03:51 +0100

    feat(moretypes): add range code and test

---
+++ moretypes/range/range_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestRangeProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "2**0 = 1\n2**1 = 2\n2**2 = 4\n2**3 = 8\n2**4 = 16\n2**5 = 32\n2**6 = 64\n2**7 = 128\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
