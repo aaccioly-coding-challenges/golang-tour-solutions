3e3a2fdd64eb4a8b5d79d86b20bd0aef62f774c8
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-21 00:48:04 +0100

    feat(moretypes): add map literals code and test

---
+++ moretypes/map-literals/map-literals_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestMapLiteralsProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "map[Bell Labs:{40.68433 -74.39967} Google:{37.42202 -122.08408}]\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
