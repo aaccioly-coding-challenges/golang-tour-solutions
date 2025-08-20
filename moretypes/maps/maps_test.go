a6342a0ca9d19ee5cca7248d877f8ec478a07049
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-21 00:43:58 +0100

    feat(moretypes): add maps code and test

---
+++ moretypes/maps/maps_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestMapsProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "{40.68433 -74.39967}\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+
+}
