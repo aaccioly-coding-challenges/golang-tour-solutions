ea20cccdaaa824ccc4688be73d8ce58c4addf21f
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-08-21 00:22:04 +0100

    feat(moretypes): add range continued code and test

---
+++ moretypes/range-continued/range-continued_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestRangeContinuedProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "1\n2\n4\n8\n16\n32\n64\n128\n256\n512\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+
+}
