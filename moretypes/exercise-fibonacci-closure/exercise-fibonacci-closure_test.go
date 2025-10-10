b30a4b77faabc8363540bfee77eb1b67ef512978
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-10-11 00:46:33 +0100

    feat(moretypes): add fibonacci closure exercise and test

---
+++ moretypes/exercise-fibonacci-closure/exercise-fibonacci-closure_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestFibonacciClosureProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "1\n1\n2\n3\n5\n8\n13\n21\n34\n55\n"
+
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
