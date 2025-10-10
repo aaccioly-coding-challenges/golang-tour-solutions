aad3f33b5d2b631642a539ee3145dc518b7d7e63
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-10-11 00:13:13 +0100

    feat(moretypes): add function values code and test

---
+++ moretypes/function-values/function-values_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestFunctionValuesProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "13\n5\n81\n"
+
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
