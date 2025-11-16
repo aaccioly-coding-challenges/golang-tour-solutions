e084abacf2828a1771d72fb8a2134892559696b5
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-11-16 23:57:23 +0000

    feat(methods): pointers and functions code and tests

---
+++ methods/methods-pointers-explained/methods-pointers-explained_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestPointersAndFunctionsProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "50\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
