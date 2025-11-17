1f142ce6f4df00f4d9b3a93051517aef94436676
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-11-17 00:27:24 +0000

    feat(methods): methods and pointer indirection code and tests

---
+++ methods/indirection/indirection_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestMethodsAndPointerIndirectionProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "{60 80} &{96 72}\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
