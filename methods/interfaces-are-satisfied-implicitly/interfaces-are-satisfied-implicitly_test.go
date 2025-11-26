0054bd47fc09e81fc47e06faa8b418e6f2cde37a
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-11-26 22:16:11 +0000

    feat(methods): add interfaces are implemented implicitly code and tests

---
+++ methods/interfaces-are-satisfied-implicitly/interfaces-are-satisfied-implicitly_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestInterfacesAreSatisfiedImplicitlyProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "hello\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+
+}
