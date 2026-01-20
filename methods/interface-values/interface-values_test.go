f258f3e7c052c6210ff891536c585cab8ad56122
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2026-01-20 23:58:22 +0000

    feat(methods): add interface values code and tests

---
+++ methods/interface-values/interface-values_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestInterfaceValues(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "(&{Hello}, *main.T)\nHello\n(3.141592653589793, main.F)\n3.141592653589793\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
