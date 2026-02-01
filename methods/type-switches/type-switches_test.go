b2ec615b7ce45b38d884b7987a917afeb78a7d67
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2026-02-01 23:28:51 +0000

    feat(methods) add type switches code and tests

---
+++ methods/type-switches/type-switches_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestTypeSwitchesProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "Twice 21 is 42\n\"hello\" is 5 bytes long\nI don't know about type bool!\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
