64cc960ec4f07294211b46dee61c29b462b005b5
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2026-02-02 21:54:59 +0000

    feat(methods) add readers code and tests

---
+++ methods/reader/reader_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestReaderProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := "n = 8, err = <nil>, b = [72 101 108 108 111 44 32 82]\nb[:n] = \"Hello, R\"\nn = 6, err = <nil>, b = [101 97 100 101 114 33 32 82]\nb[:n] = \"eader!\"\nn = 0, err = EOF, b = [101 97 100 101 114 33 32 82]\nb[:n] = \"\"\n"
+
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
