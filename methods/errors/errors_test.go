9a12926275b5da2d41a1cb2c0faffef55bc7617c
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2026-02-02 00:00:54 +0000

    feat(methods) add errors code and tests

---
+++ methods/errors/errors_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestErrorsProgram(t *testing.T) {
+	output := testutils.CaptureMainOutput(main)
+	expected := `^at \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+ \+\d{4} \w+ m=\+\d+\.\d+, it didn't work\n$`
+
+	if match, _ := regexp.MatchString(expected, output); !match {
+		t.Errorf("Expected output %q to match pattern %q", output, expected)
+	}
+
+}
