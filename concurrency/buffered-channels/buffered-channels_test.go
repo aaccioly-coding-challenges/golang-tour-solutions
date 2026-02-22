5d401191779ba738c134cbad13bf68ac57a4e70f
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2026-02-22 23:31:05 +0000

    feat(concurrency) add buffered channels code and tests

---
+++ concurrency/buffered-channels/buffered-channels_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/testutils"
+)
+
+func TestBufferChannelsProgram(t *testing.T) {
+	output := testutils.CaptureOutput(main)
+	expected := "1\n2\n"
+
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
