c8c43fb9e3241599f1842c14ede0ed1224d9e58c
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2026-03-22 00:02:04 +0000

    feat(concurrency) add mutex counter code and tests

---
+++ concurrency/mutex-counter/mutex-counter_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/aaccioly-coding-challenges/golang-tour-solutions/internal/capture"
+)
+
+func TestMutextCounterProgram(t *testing.T) {
+	output := capture.CaptureOutput(main)
+	expected := "1000\n"
+	if output != expected {
+		t.Errorf("Expected output %q, got %q", expected, output)
+	}
+}
